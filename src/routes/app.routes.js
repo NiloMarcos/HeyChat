81ff10adc1de48e2f73f5f228863c23800bba29d
Nilo Marcos <nilo.n2m15@gmail.com>
2021-10-10 15:24:19 -0300

    implement of navigation for app

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import SignIn from '../screens/SignIn';
+import ChatRoom from '../screens/ChatRoom';
+import Messages from '../screens/Messages';
+import Search from '../screens/Search';
 
 const AppStack = createNativeStackNavigator();
 
 
 export default function AppRoutes(){
   return(
-    <AppStack.Navigator>
+    <AppStack.Navigator initialRouteName="ChatRoom">
       <AppStack.Screen 
         name="SignIn" 
         component={SignIn}
         options={{
           title: 'Faca seu login'
+        }}
+        />
+      <AppStack.Screen 
+        name="ChatRoom" 
+        component={ChatRoom}
+        options={{
+          headerShown: false,
         }}
         />
     </AppStack.Navigator>
